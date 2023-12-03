b1915ee77d159e9410ae1e9e0866c389d5f89fef
Slaieet <sl4ieet@gmail.com>
2023-12-03 15:13:34 -0600

    Commit del proyecto

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue';
+
+
+  let num = ref(0)
+
+  const sumar = () => {
+    num.value += 1
+  }
+
+</script>
+
+<template>
+  <main>
+    <h1>Proyecto de vue js creado con vite usando npm</h1>
+    <button @click="sumar">Contador: {{ num }}</button>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 2rem;
+}
+
+</style>
